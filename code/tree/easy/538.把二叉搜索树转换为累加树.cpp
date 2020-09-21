52fb634b917d495ea2a47c66180d2c14a9664e60
PurpleSwords <38780499+PurpleSwords@users.noreply.github.com>
2020-09-22 02:07:26 +0800

    同步题目描述

--- code/tree/easy/538.把二叉搜索树转换为累加树.cpp
+++ code/tree/easy/538.把二叉搜索树转换为累加树.cpp
@@ -2,6 +2,40 @@
  * @lc app=leetcode.cn id=538 lang=cpp
  *
  * [538] 把二叉搜索树转换为累加树
+ *
+ * https://leetcode-cn.com/problems/convert-bst-to-greater-tree/description/
+ *
+ * algorithms
+ * Easy (64.04%)
+ * Likes:    390
+ * Dislikes: 0
+ * Total Accepted:    61.7K
+ * Total Submissions: 96.4K
+ * Testcase Example:  '[5,2,13]'
+ *
+ * 给定一个二叉搜索树（Binary Search Tree），把它转换成为累加树（Greater
+ * Tree)，使得每个节点的值是原来的节点值加上所有大于它的节点值之和。
+ * 
+ * 
+ * 
+ * 例如：
+ * 
+ * 输入: 原始二叉搜索树:
+ * ⁠             5
+ * ⁠           /   \
+ * ⁠          2     13
+ * 
+ * 输出: 转换为累加树:
+ * ⁠            18
+ * ⁠           /   \
+ * ⁠         20     13
+ * 
+ * 
+ * 
+ * 
+ * 注意：本题和 1038:
+ * https://leetcode-cn.com/problems/binary-search-tree-to-greater-sum-tree/ 相同
+ * 
  */
 
 // @lc code=start
@@ -35,4 +69,3 @@ public:
     }
 };
 // @lc code=end
-
