f2e051afef565e0cb1a15089ba8c2f62db7d92d6
PurpleSwords <38780499+PurpleSwords@users.noreply.github.com>
2022-02-11 01:36:33 +0800

    114.二叉树展开为链表

---
+++ code/tree/medium/114.二叉树展开为链表.cpp
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=114 lang=cpp
+ *
+ * [114] 二叉树展开为链表
+ *
+ * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/description/
+ *
+ * algorithms
+ * Medium (72.48%)
+ * Likes:    1057
+ * Dislikes: 0
+ * Total Accepted:    212.6K
+ * Total Submissions: 292.1K
+ * Testcase Example:  '[1,2,5,3,4,null,6]'
+ *
+ * 给你二叉树的根结点 root ，请你将它展开为一个单链表：
+ * 
+ * 
+ * 展开后的单链表应该同样使用 TreeNode ，其中 right 子指针指向链表中下一个结点，而左子指针始终为 null 。
+ * 展开后的单链表应该与二叉树 先序遍历 顺序相同。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,2,5,3,4,null,6]
+ * 输出：[1,null,2,null,3,null,4,null,5,null,6]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [0]
+ * 输出：[0]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中结点数在范围 [0, 2000] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你可以使用原地算法（O(1) 额外空间）展开这棵树吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        while(root) {
+            auto p = root->left;
+            if(p) {
+                // 找到左子树的最右结点，将其转移到右子树上
+                while(p->right) {
+                    p = p->right;
+                }
+                p->right = root->right;
+                
+                // 将左子树搬到右子树上并保持原样，相当于将左子树上的右边平移到右子树上
+                root->right = root->left;
+                root->left = nullptr;
+            }
+            // 没有左子树，继续检查下一个节点
+            root=root->right;
+        }
+    }
+};
+// @lc code=end
+
